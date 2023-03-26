0616827f3252da8ac986ac3612d5610e05dd0d4f
nick0413 <68749910+nick0413@users.noreply.github.com>
2023-03-26 14:46:02 -0500

    Map Generation
    Codigo que genera un grafo al azar completamente conectado para pruebas del programa

---
+++ test_data.py
@@ -0,0 +1,36 @@
+import numpy as np
+import random as rd
+import networkx as nx
+import matplotlib.pylab as plt
+
+def gen_graph():
+	while True:
+		print('----------')
+		Msize=rd.randint(5,20)
+		M=np.random.randint(0,10,(Msize,Msize))
+		#print(M)
+		for i in range(Msize):
+			for j in range(Msize):
+				if(rd.randint(0,100)>10):
+					M[i,j]=0
+		G=nx.from_numpy_array(M)
+
+		G.remove_edges_from(list(nx.selfloop_edges(G)))
+
+		if not (G.is_multigraph()):
+			print('multigraph',G.is_multigraph())
+			if nx.is_connected(G):
+				if nx.number_of_isolates(G)==0:
+					print('no isolates')
+					break
+	return G
+
+G1=gen_graph()	
+
+
+
+M1=nx.to_numpy_array(G1)
+G1=nx.from_numpy_array(M1)
+
+nx.draw(G1, with_labels = True)
+plt.show()
