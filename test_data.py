ab80f2f71ba1c24a66be7645970151e304b7cf97
nick0413 <68749910+nick0413@users.noreply.github.com>
2023-03-26 15:09:20 -0500

    Update test_data.py

--- test_data.py
+++ test_data.py
@@ -6,7 +6,7 @@ import matplotlib.pylab as plt
 def gen_graph():
 	while True:
 		print('----------')
-		Msize=rd.randint(5,20)
+		Msize=rd.randint(15,30)
 		M=np.random.randint(0,10,(Msize,Msize))
 		#print(M)
 		for i in range(Msize):
@@ -32,5 +32,12 @@ G1=gen_graph()
 M1=nx.to_numpy_array(G1)
 G1=nx.from_numpy_array(M1)
 
-nx.draw(G1, with_labels = True)
+
+edge_colors = M1[np.triu_indices_from(M1)]
+edges=np.ones((edge_colors.shape[0],4))
+print(edge_colors)
+pos = nx.spring_layout(G1)
+#nx.draw(G1, pos=pos, with_labels=True, edge_color=edges)
+print(M1)
+nx.draw(G1,with_labels = True)
 plt.show()
