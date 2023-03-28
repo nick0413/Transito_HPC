ef9e185691fddf76b6380d35fa4db34f98a396de
esteban11519 <eladinof@unal.edu.co>
2023-03-28 14:54:23 -0500

    Adelanto GUI

--- GUI_SFML/main.cpp
+++ GUI_SFML/main.cpp
@@ -1,13 +1,38 @@
-#include <SFML/Window.hpp>
+#include <string>
+#include <iostream>
 
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+
+
+using namespace std;
 int main(){
-    
+
+    string figFondo= "./figs/Unal-CU-Bogota.jpg";
+
     // Se inicializa una ventana de 800x600 con el título TransitoHPC 
     // El tercer argumento es el estilo que por defecto muestra
     // un título, redimensionamiento y para cerrar.
-    sf::Window window(sf::VideoMode(800,600),"TransitoHPC");
+    sf::RenderWindow window(sf::VideoMode(800,600),"TransitoHPC");
+
+    sf::Texture textFondo;
+
+    // Se crea la textura
+    if(!textFondo.loadFromFile(figFondo))
+    {
+        cout<< "por favor cargue verifique la ruta: "<<figFondo<<endl;
+    }
+
+    // Se crea el fondo como Sprite
+    sf::Sprite sprFondo;
+
+    //  Sprite
+    sprFondo.setTexture(textFondo);
+
 
     while(window.isOpen()){
+       
         sf::Event event;
         //Se mantiene en el loop si algún evento pasa
         while (window.pollEvent(event))
@@ -17,6 +42,13 @@ int main(){
                 window.close();
             }
         }
+        // Es una forma de actualizar
+        window.clear();
+        
+        window.draw(sprFondo);
+
+        window.display();
+
     }
 
     return 0;
