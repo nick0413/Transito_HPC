d701a0a85577ec154d111efa0f2cfdae1efa6139
esteban11519 <eladinof@unal.edu.co>
2023-03-28 23:55:08 -0500

    Pantalla completa con la imagen compartida por la Universidad

--- GUI_SFML/main.cpp
+++ GUI_SFML/main.cpp
@@ -9,26 +9,40 @@
 using namespace std;
 int main(){
 
-    string figFondo= "./figs/Unal-CU-Bogota.jpg";
-
+    string figFondo= "./figs/Contenedores-Residuos.jpg";
     // Se inicializa una ventana de 800x600 con el título TransitoHPC 
     // El tercer argumento es el estilo que por defecto muestra
     // un título, redimensionamiento y para cerrar.
     sf::RenderWindow window(sf::VideoMode(800,600),"TransitoHPC");
-
     sf::Texture textFondo;
+    // Se crea el fondo como Sprite
+    sf::Sprite sprFondo;
+
+    // Tomado de: https://stackoverflow.com/questions/36448101/2-3-1-set-scale-of-background-texture-to-renderwindow-size
+    sf::Vector2u textureSize;  //Added to store texture size.
+    sf::Vector2u windowSize;   //Added to store window size.
+
+    
 
     // Se crea la textura
     if(!textFondo.loadFromFile(figFondo))
     {
         cout<< "por favor cargue verifique la ruta: "<<figFondo<<endl;
     }
+    // Tomado de: https://stackoverflow.com/questions/36448101/2-3-1-set-scale-of-background-texture-to-renderwindow-size
 
-    // Se crea el fondo como Sprite
-    sf::Sprite sprFondo;
+    textureSize = textFondo.getSize(); //Get size of texture.
+    windowSize = window.getSize();             //Get size of window.
+
+    float scaleX = (float) windowSize.x / textureSize.x;
+    float scaleY = (float) windowSize.y / textureSize.y;     //Calculate scale.
+
 
     //  Sprite
     sprFondo.setTexture(textFondo);
+
+    // Tomado de: https://stackoverflow.com/questions/36448101/2-3-1-set-scale-of-background-texture-to-renderwindow-size
+    sprFondo.setScale(scaleX, scaleY);
 
 
     while(window.isOpen()){
