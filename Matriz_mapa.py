322020b6022327795ac496327c2edbf05cedfc71
nick0413 <68749910+nick0413@users.noreply.github.com>
2023-04-10 17:37:23 -0500

    Dijkstra funcional

--- Matriz_mapa.py
+++ Matriz_mapa.py
@@ -12,7 +12,7 @@ for i in range(datos.shape[0]):
   Madyacencia[int(datos[i,0])-1,int(datos[i,1]-1)]=np.sqrt(datos[i,2]**2+datos[i,3]**2)
 
 Madyacencia=(Madyacencia+Madyacencia.T).astype("int32")
-Madyacencia[Madyacencia==0]=999
+Madyacencia[Madyacencia==0]=9999
 np.fill_diagonal(Madyacencia, 0)
 plt.imshow(Madyacencia)
 np.savetxt("Madyacencia.txt",Madyacencia,delimiter=',')
---
+++ test/testpy.py
@@ -0,0 +1,18 @@
+import numpy as np
+import networkx as nx
+import matplotlib.pylab as plt
+import sys 
+
+start=int(sys.argv[1])
+end=int(sys.argv[2])
+
+arr=np.loadtxt("Madyacencia.txt",delimiter=',')
+
+arr[arr==9999]=0
+
+plt.imshow(arr)
+plt.show()
+G=nx.from_numpy_array(arr)
+nx.draw(G,with_labels = True)
+plt.show()
+print(nx.shortest_path(G,source=start,target=end, weight='weight'))
