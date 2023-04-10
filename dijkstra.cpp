a51ead0fb9e7aab3bae75b76d36ef7e55eb9cdb2
nick0413 <68749910+nick0413@users.noreply.github.com>
2023-04-10 13:42:42 -0500

    ejemplo del algoritmo semifuncional

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
-
-// #include "Nodo.h"
-#include "Camion.h"
 #include <fstream>
 #include <armadillo>
+#include <string>
 
 using namespace std;
 int V;
 int cost[100][100];
+
+arma::vec append_vec(arma::vec & vector, double value)
+	{
+		vector.resize(vector.n_elem+1);
+		vector(vector.n_elem-1)=value;
+		return vector;
+	}
+arma::uvec append_vec(arma::uvec & vector, int value)
+	{
+		vector.resize(vector.n_elem+1);
+		vector(vector.n_elem-1)=value;
+		return vector;
+	}
 
 arma::mat load_csv_arma (const std::string & path) {
     arma::mat X;
@@ -23,6 +34,15 @@ arma::vec in_dist(int size, int value, int source)
 			{dist(i)=value;}
 		dist(source)=0;
 		return dist;
+	}
+
+arma::uvec in_prev(int size)
+	{
+		arma::uvec prev;
+		prev.resize(size);
+		for (int i=0; i<size;i++)
+			{prev(i)=i;}
+		return prev;
 	}
 
 int get_nearest(int V,arma::vec dist, bool  *visited){
@@ -41,26 +61,66 @@ int get_nearest(int V,arma::vec dist, bool  *visited){
 
 }
 
-void dijkstra(int V,int source)
+arma::uvec dijkstra(arma::mat Mapa,int V,int source)
 	{	
 		bool visited[V]={0};
+	
+		arma::uvec prev=in_prev(V);
 		arma::vec dist=in_dist(V,999,source);
 		for(int ii=0; ii<V-1; ii++)
 			{
-				int neares_unvisited= get_nearest(V,dist,visited)
+				int neares_unvisited= get_nearest(V,dist,visited);
+				visited[neares_unvisited]= true;
+
+				for( int adj =0; adj<V; adj++)
+					{
+						if (Mapa(neares_unvisited,adj) != 999)
+							{if(dist(adj)<dist(neares_unvisited)+Mapa(neares_unvisited,adj))
+								{
+									dist(adj)=dist(neares_unvisited)+Mapa(neares_unvisited,adj);
+									prev[adj]=neares_unvisited;
+									cout<<"Se cumplio\n";
+								}
+							}
+
+					}
+
 			}
+		return prev;
+		
+	}
+
+arma::uvec find_path(arma::mat Mapa,int source, int target)
+	{	
+
+		int V=Mapa.n_rows;
+		arma::uvec prev = dijkstra(Mapa,V,source);
+		arma::uvec path;
+		path.resize(1);
+		path=append_vec(path,target);
+		int node=target;
+		for(int jj=0;jj<prev.size();jj++)
+			{cout<<prev(jj)<<" "<<jj<<"\n";}
+		// while (node!=source)
+		// 	{
+		// 		node=prev[node];
+		// 		path=append_vec(path,node);
+		// 		cout<<node<<"\n";
+		// 	}
+		return reverse(path);
+
+
 	}
 
 int main(){
 
-	arma::mat Mapa = load_csv_arma("Madyacencia.txt");
-	int V=Mapa.n_rows;
-	int source=3;
-
+	arma::mat Mapa = load_csv_arma("matriz23.csv");
 	
+	int source=1;
+	int target=7;
 
-	
+	arma::uvec path =find_path(Mapa,source,target);
 
-
+	return 0;
 
 }
