3b5710ebe824ae5bccbea50e2b49f9ebe72c4584
esteban11519 <eladinof@unal.edu.co>
2023-04-11 12:45:58 -0500

    Todo actualizado

---
+++ test/Tools/test.cpp
@@ -0,0 +1,34 @@
+#include "Tools.h"
+
+int main(){
+  
+
+  // vector<vector<string>> datosContenedores;
+  // vector<Contenedor> misContenedores;
+  
+  // string fname="datos_contenedores.txt";
+
+  Tools tools;
+  
+  // tools.datosContenedores(fname,datosContenedores);
+
+  // tools.vectorContenedores(fname,misContenedores);
+
+  // for(auto cont : misContenedores){
+  //   cout<<"pos x: "<<cont.getPosition().x<<endl;
+  //   cout<<"pos y: "<<cont.getPosition().y<<endl;
+  //   cout<<"radius: "<<cont.getRadius()<<endl<<endl;
+  // }
+
+  
+  string file="nodos_carretera.txt";
+  vector<NodosCarretera> nodosCarretera;
+
+  tools.vectorNodosCarretera(file,nodosCarretera);
+
+  
+
+  
+  return 0;
+}
+
